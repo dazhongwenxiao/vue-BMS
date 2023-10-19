feda2e9dfa93221aa989aa8b86f88efb1a4e96ef
szw <2574387495@qq.com>
2023-10-19 16:34:50 +0800

    登录功能完成

--- src/api/user.js
+++ src/api/user.js
@@ -1,24 +1,27 @@
 import request from '@/utils/request'
 
+// 对外暴露登录接口函数 POST /admin/acl/index/login
 export function login(data) {
   return request({
-    url: '/vue-admin-template/user/login',
+    url: '/admin/acl/index/login',
     method: 'post',
     data
   })
 }
 
+// 对外暴露获取用户信息函数 GET /admin/acl/index/info
 export function getInfo(token) {
   return request({
-    url: '/vue-admin-template/user/info',
+    url: '/admin/acl/index/info',
     method: 'get',
     params: { token }
   })
 }
 
+// 对外暴露登录函数
 export function logout() {
   return request({
-    url: '/vue-admin-template/user/logout',
+    url: '/admin/acl/index/logout',
     method: 'post'
   })
 }
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="login-container">
+    <!-- el-form组件：elementUI插件里面的一个组件，经常展示表单元素 -->
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">请先登录</h3>
       </div>
 
       <el-form-item prop="username">
@@ -41,7 +42,7 @@
         </span>
       </el-form-item>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
+      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
 
       <div class="tips">
         <span style="margin-right:20px;">username: admin</span>
@@ -74,8 +75,8 @@ export default {
     }
     return {
       loginForm: {
-        username: 'admin',
-        password: '111111'
+        username: 'admin', // admin
+        password: '111111' // 111111
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
@@ -105,11 +106,17 @@ export default {
         this.$refs.password.focus()
       })
     },
+    // 登录业务：发请求，带着用户名和密码给服务器
     handleLogin() {
+      // 这里是验证表单元素（用户名与密码）是否符合规则
       this.$refs.loginForm.validate(valid => {
+        // 如果符合规则
         if (valid) {
+          // 按钮会有一个loading效果
           this.loading = true
+          // 派发一个action请求，带着用户名与密码
           this.$store.dispatch('user/login', this.loginForm).then(() => {
+            // 成功后进行路由跳转
             this.$router.push({ path: this.redirect || '/' })
             this.loading = false
           }).catch(() => {
